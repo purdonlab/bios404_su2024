a1634057bf4e076e3685db384a5ef729bc7225d3
tomdstone <77251489+tomdstone@users.noreply.github.com>
2024-07-31 13:42:05 -0700

    Had forgotten week 1 solutions, tiny update to wording in lab2

--- lab1/utils.py
+++ lab1/utils.py
@@ -1,5 +1,4 @@
-# Authors: Proloy Das <email:proloyd94@gmail.com>
-#          Tom Stone <email:tomstone@stanford.edu>
+# Author: Proloy Das <email:proloyd94@gmail.com>
 # License: BSD (3-clause) 
 import numpy as np
 from scipy import linalg, signal, fft
@@ -24,20 +23,59 @@ def compute_theoritical_ar_acov(phi, p):
     times = np.arange(-p, p+1)
     return acov, times
 
+def autocovariance(timeseries, mode='same'):
+    """
+    Fill in this function! It should be able to handle shifts of varying lengths.
+    """
+    N = len(timeseries)
 
-# Copy the function definition of`compute_autocovaraince()` to here, and
-# remove these commented line.
-# You shall keep in mind that two function definitions should be spaced by
-# two blank lines.
+    sample_autocov = signal.correlate(timeseries, timeseries, mode=mode) / N
+    if mode == 'same':
+        shifts = np.arange(-N//2, N//2)
+    elif mode == 'full':
+        shifts = np.arange(-N, N)
+    else:
+        shifts = None
+
+    return sample_autocov, shifts
 
 
-# Copy the funtion definition `compute_periodogran` to here, and remove
-# these commented line.
+def periodogram(timeseries, fs = 1):
+    
+    psd = fft.fft(timeseries)
+    freqs = np.linspace(0., fs, num=len(psd))
+
+    return np.abs(psd)**2 / len(timeseries), freqs
+
+def periodogram_tapered(timeseries, taper, fs=1):
+    
+    sig = timeseries * taper
+    return periodogram(sig, fs=fs)
 
 
-# Copy the funtion definition `compute_tapered_periodogran` to here, and remove
-# these commented line.
+def multitaper_periodogram(timeseries, nw=4, ntapers = 7, fs = 1):
+    tapers = signal.windows.dpss(len(timeseries), nw, Kmax=ntapers) * np.sqrt(len(timeseries))
 
+    # Loop through tapers to compute different spectral estimates
+    powers = []
+    for taper in tapers:
+        powers.append(periodogram_tapered(timeseries, taper)[0])
 
-# Copy the funtion definition `compute_multitaper_spectrum` to here, and remove
-# these commented line.
+    _, freqs = periodogram(timeseries, fs=fs)
+    S_yy_est = np.asanyarray(powers)
+    power = S_yy_est.mean(axis=0)
+    return power, freqs
+
+def multitaper_spectrogram(timeseries, window_size = 1024, nw = 4, ntapers = 4, fs=1):
+    N = len(timeseries)
+
+    nwindows = N // window_size
+    spectrogram = np.zeros((window_size, nwindows))
+
+    for i in range(nwindows):
+        spectrogram[:,i] = multitaper_periodogram(timeseries[i*window_size:(i+1)*window_size], nw=nw, ntapers=ntapers, fs=fs)[0]
+
+    freqs = np.linspace(0, fs, num=window_size)
+    times = np.linspace(0, num=nwindows, stop=nwindows*(window_size / fs))
+
+    return spectrogram, freqs, times
